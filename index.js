f8b27d3ee9c1d89fcdc5c363514072d9a3701875
Jacob Smith <jacob@jacobsmith.io>
2017-01-26 23:10:02 -0500

    Create new connection on each method call

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const url   = require('url');
 class FtpHttpAdapter {
   constructor(options) {
     this.options = processOptions(options);
-    this.ftpClient = new Ftp();
+  }
 
-    this.ftpConnect = new Promise((resolve) => {
-      this.ftpClient.on('ready', resolve);
-    });
+  _connect() {
+    this.ftpClient = new Ftp();
 
     if(this.options.debug) {
       this.ftpClient.on('greeting', console.log); // eslint-disable-line no-console
@@ -19,45 +18,58 @@ class FtpHttpAdapter {
       this.ftpClient.on('error', console.error); // eslint-disable-line no-console
     }
 
+
+    let promise = new Promise((resolve) => {
+      this.ftpClient.on('ready', resolve);
+    });
+
     this.ftpClient.connect(this.options.ftp);
+
+    return promise;
+  }
+
+  _disconnect(val) {
+    this.ftpClient.end();
+    delete this.ftpClient;
+    return val;
   }
 
   createFile(filename, data) {
     const buffer = Buffer.from(data);
     const filePath = path.join(this.options.ftp.path, filename);
 
-    return this.ftpConnect.then(() => {
+    return this._connect().then(() => {
       return new Promise((resolve, reject) => {
         this.ftpClient.put(buffer, filePath, function(err) {
           if(err) return reject(err);
           resolve();
         });
       });
-    });
+    }).then(this._disconnect.bind(this));
   }
 
   getFileData(filename) {
     const filePath = path.join(this.options.ftp.path, filename);
 
-    return this.ftpConnect.then(() => {
+    return this._connect().then(() => {
       return new Promise((resolve, reject) => {
         this.ftpClient.get(filePath, function(err, stream) {
           if(err) return reject(err);
-          resolve(streamToBuffer(stream));
+          streamToBuffer(stream).then(resolve, reject);
         });
       });
-    });
+    }).then(this._disconnect.bind(this));
   }
 
   deleteFile(filename) {
-    return this.ftpConnect.then(() => {
+    return this._connect().then(() => {
       return new Promise((resolve, reject) => {
         this.ftpClient.delete(path.join(this.options.ftp.path, filename), function(err) {
           if(err) return reject(err);
-          resolve();
+          return resolve();
         });
       });
-    });
+    }).then(this._disconnect.bind(this));
   }
 
 
